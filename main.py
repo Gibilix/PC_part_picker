f6ef7209c1f83114e87c1821e9f6aecf92f62edc
Gibilix <64995737+Gibilix@users.noreply.github.com>
2023-01-30 18:31:19 +0000

    Added Cpu class

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
-class Parts():
+class Parts:
     pass
+class Cpu:
+    def __init__(self, use, price, performance, tdp, igpu, p_cores, e_cores, threads, OC, DDR4, DDR5, cooler):
+        self.use = use
+        self.price = price
+        self.performance = performance
+        self.tdp = tdp
+        self.igpu = igpu
+        self.p_cores = p_cores
+        self.e_cores = e_cores
+        self.threads = threads
+        self.OC = OC
+        self.DDR4 = DDR4
+        self.DDR5 = DDR5
+        self.cooler = cooler
+class Gpu:
+    def __init__(s):
+
+class storage:
+    pass
+
+"""def __init__(self, cpu, gpu, storage, ram, case, psu):
+        self.cpu = cpu
+        self.gpu = gpu
+        self.storage = storage
+        self.ram = ram
+        self.case = case
+        self.psu = psu
+    def __str__():"""
+        
+
